c06ed3fb737de02ba5607d2669d966cbb712dcae
Thomas Wang <wglcoco@gmail.com>
2021-06-17 22:00:04 +0800

    [Thomas] Remove redundant code

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
@@ -1,16 +1,10 @@
 package com.tw.academy.basic.$6_primitive_obsession.practiceOne;
 
 public class DeliveryManager {
-    String toAddress;
-    String fromAddress;
-
     private Address from;
     private Address to;
 
     public DeliveryManager(String fromAddress, String toAddress) {
-        this.toAddress = toAddress;
-        this.fromAddress = fromAddress;
-
         this.from = new Address(fromAddress);
         this.to = new Address(toAddress);
     }
